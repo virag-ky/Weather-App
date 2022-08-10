6f177598396fb963b8c78be84893d56f67234f60
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-10 17:30:56 +0300

    Add top part of the current weather component

---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,15 @@
+import '../../styles/current-weather.css';
+
+const CurrentWeather = () => (
+  <div className="weather">
+    <div className="top">
+      <div>
+        <p className="city">Belgrade</p>
+        <p className="weather-description">Sunny</p>
+      </div>
+      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+    </div>
+  </div>
+);
+
+export default CurrentWeather;
