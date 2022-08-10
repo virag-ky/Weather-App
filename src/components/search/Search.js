9f66a93e43e11e38b639f22e26f8ea6ddd3d50db
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-10 16:55:53 +0300

    Fetch cities functionality

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,23 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { AsyncPaginate } from 'react-select-async-paginate';
+import { GEO_API_URL, geoApiOptions } from '../../api';
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => fetch(
+    `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+    geoApiOptions,
+  )
+    .then((response) => response.json())
+    .then((response) => ({
+      options: response.data.map((city) => ({
+        value: `${city.latitude} ${city.longitude}`,
+        label: `${city.name}, ${city.countryCode}`,
+      })),
+    }))
+    .catch((err) => console.error(err));
 
   const handleChange = (data) => {
     setSearch(data);
@@ -16,6 +30,7 @@ const Search = ({ onSearchChange }) => {
       debounceTimeout={600}
       value={search}
       onChange={handleChange}
+      loadOptions={loadOptions}
     />
   );
 };
