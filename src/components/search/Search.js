2d670e13d733d8c73cc5cc9e7434a624b18d68d7
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-11 16:09:58 +0300

    Add min population

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,19 +1,23 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { AsyncPaginate } from 'react-select-async-paginate';
-import { GEO_API_URL, geoApiOptions } from '../../api';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({ onSearchChange }) => {
   const [search, setSearch] = useState(null);
 
-  const loadOptions = (inputValue) => fetch(`${GEO_API_URL}/cities?&namePrefix=${inputValue}`, geoApiOptions)
-    .then((response) => response.json())
-    .then((response) => ({
-      options: response.data.map((city) => ({
-        value: `${city.latitude} ${city.longitude}`,
-        label: `${city.name}, ${city.countryCode}`,
-      })),
-    }));
+  const loadOptions = (inputValue) =>
+    fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => ({
+        options: response.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        })),
+      }));
 
   const handleChange = (data) => {
     setSearch(data);
