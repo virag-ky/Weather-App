7a20b03ec1fb0df8680c1f78179065c579ccd398
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-11 21:25:53 +0300

    Replace API key

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+    'X-RapidAPI-Key': 'a3ab59d1b2msh219712e4e3505d1p11cc7ajsnf2ed80529c2e',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 };
